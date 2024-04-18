5248ce4ad62ee13c183cbbdd08f4ad0b787a30ed
Pablo <pablomolinarojas@gmail.com>
2024-04-18 19:11:56 -0400

    fetch, partition, read reg, sign extend

--- src/project.c
+++ src/project.c
@@ -12,7 +12,10 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
+	if((PC >> 2) % 4 != 0) return 1;
 
+	instruction = Mem[PC >> 2];
+	return 0;
 }
 
 
@@ -20,7 +23,13 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-
+	*op = instruction >> 26;
+	*r1 = instruction >> 21 & ((1 << 5)-1);
+	*r2 = instruction >> 16 & ((1 << 5)-1);
+	*r3 = instruction >> 11 & ((1 << 5) -1);
+	*funct= instruction  & ((1 << 6) -1);
+	*offset = instruction & ((1 << 5) - 1);
+	*jsec == instruction  & ((1 << 26) -1);
 }
 
 
@@ -29,14 +38,23 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+	controls->RegDst = 0;
+	controls->Jump = 0;
+	controls->Branch = 0;
+	controls->MemRead = 0;
+	controls->MemtoReg = 0;
+	controls->ALUOp = 0;
+	controls->MemWrite = 0;
+	controls->ALUSrc = 0;
+	controls->RegWrite = 0;
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+	*data1 = Reg[r1];
+	*data2 = Reg[r2];
 }
 
 
@@ -44,7 +62,7 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+	*extended_value = (offset >> 15) == 1;
 }
 
 /* ALU operations */
